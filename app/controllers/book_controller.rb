44b6b13d83518185fe2b29552cc4db7cb6c6ab9b
Filipe <filipegoncalves.ti@gmail.com>
2019-06-24 00:53:44 -0300

    adicionado ordenação asc e desc dos três campos

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,21 @@
 class BookController < ApplicationController
-    def index
-        @books = Book.all
-    end
+    helper_method :sort_column, :sort_direction
     
+    def index
+        @books = Book.order("#{sort_column} #{sort_direction}")
+    end
+
+    private
+
+    def sort_column
+        Book.column_names.include?(
+        params[:sort]) ? 
+        params[:sort] : "title"
+    end
+
+    def sort_direction
+        %w[asc desc].include?(
+        params[:direction]) ? 
+        params[:direction] : "asc"
+    end   
 end
